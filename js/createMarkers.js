cb72347d16f24472539cf99fa98c2e4cc581ce66
Fun Planet <dipamdiptam@gmail.com>
2021-10-01 19:59:46 +0530

    menu

--- js/createMarkers.js
+++ js/createMarkers.js
@@ -14,65 +14,106 @@ AFRAME.registerComponent("create-markers", {
       marker.setAttribute("marker-handler", {});
       mainScene.appendChild(marker);
 
-      const model = document.createElement("a-entity");
-      model.setAttribute("id", `model-${dish.id}`);
-      model.setAttribute("position", dish.model_geometry.position);
-      model.setAttribute("rotation", dish.model_geometry.rotation);
-      model.setAttribute("scale", dish.model_geometry.scale);
-      model.setAttribute("gltf-model", `url(${dish.model_url})`);
-      model.setAttribute("gesture-handler", {});
-      marker.appendChild(model);
+      const todaysDate = new Date();
+      const todaysDay = todaysDate.getDay();
+      const days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
 
-      const mainPlane = document.createElement("a-plane");
-      mainPlane.setAttribute("id", `mainplane-${dish.id}`);
-      mainPlane.setAttribute("position", {
-        x: 0,
-        y: 0,
-        z: 0,
-      });
-      mainPlane.setAttribute("rotation", {
-        x: -90,
-        y: 0,
-        z: 0,
-      });
-      mainPlane.setAttribute("width", 1.7);
-      mainPlane.setAttribute("height", 1.5);
-      marker.appendChild(mainPlane);
+      if (!dish.unavailable_days.includes(days[todaysDay])) {
+        const model = document.createElement("a-entity");
+        model.setAttribute("id", `model-${dish.id}`);
+        model.setAttribute("position", dish.model_geometry.position);
+        model.setAttribute("rotation", dish.model_geometry.rotation);
+        model.setAttribute("scale", dish.model_geometry.scale);
+        model.setAttribute("gltf-model", `url(${dish.model_url})`);
+        model.setAttribute("gesture-handler", {});
+        model.setAttribute("visible", false);
+        marker.appendChild(model);
 
-      const title = document.createElement("a-entity");
-      title.setAttribute("id", `title-${dish.id}`);
-      title.setAttribute("position", { x: 0, y: 0, z: 0.1 });
-      title.setAttribute("rotation", { x: 0, y: 0, z: 0 });
-      title.setAttribute("text", {
-        font: "monoid",
-        color: "black",
-        width: 1.8,
-        height: 1,
-        align: "center",
-        value: dish.dish_name.toUpperCase(),
-      });
-      mainPlane.appendChild(title);
+        const mainPlane = document.createElement("a-plane");
+        mainPlane.setAttribute("id", `mainplane-${dish.id}`);
+        mainPlane.setAttribute("position", {
+          x: 0,
+          y: 0,
+          z: 0,
+        });
+        mainPlane.setAttribute("rotation", {
+          x: -90,
+          y: 0,
+          z: 0,
+        });
+        mainPlane.setAttribute("width", 1.7);
+        mainPlane.setAttribute("height", 1.5);
+        marker.appendChild(mainPlane);
 
-      const ingrList = document.createElement("a-entity");
-      ingrList.setAttribute("id", `ingred-${dish.id}`);
-      ingrList.setAttribute("position", {
-        x: 0.3,
-        y: 0,
-        z: 0.1,
-      });
-      ingrList.setAttribute("rotation", {
-        x: 0,
-        y: 0,
-        z: 0,
-      });
-      ingrList.setAttribute("text", {
-        font: "monoid",
-        color: "red",
-        width: 2,
-        align: "left",
-        value: `${dish.ingredients.join("\n\n")}`,
-      });
-      mainPlane.appendChild(ingrList);
+        const title = document.createElement("a-entity");
+        title.setAttribute("id", `title-${dish.id}`);
+        title.setAttribute("position", { x: 0, y: 0, z: 0.1 });
+        title.setAttribute("rotation", { x: 0, y: 0, z: 0 });
+        title.setAttribute("text", {
+          font: "monoid",
+          color: "black",
+          width: 1.8,
+          height: 1,
+          align: "center",
+          value: dish.dish_name.toUpperCase(),
+        });
+        mainPlane.appendChild(title);
+
+        const ingrList = document.createElement("a-entity");
+        ingrList.setAttribute("id", `ingred-${dish.id}`);
+        ingrList.setAttribute("position", {
+          x: 0.3,
+          y: 0,
+          z: 0.1,
+        });
+        ingrList.setAttribute("rotation", {
+          x: 0,
+          y: 0,
+          z: 0,
+        });
+        ingrList.setAttribute("text", {
+          font: "monoid",
+          color: "red",
+          width: 2,
+          align: "left",
+          value: `${dish.ingredients.join("\n\n")}`,
+        });
+        mainPlane.appendChild(ingrList);
+
+        const pricePlane = document.createElement("a-image");
+        pricePlane.setAttribute("id", `priceplane-${dish.id}`);
+        pricePlane.setAttribute(
+          "src",
+          "https://raw.githubusercontent.com/whitehatjr/menu-card-app/main/black-circle.png"
+        );
+        pricePlane.setAttribute("width", 0.8);
+        pricePlane.setAttribute("height", 0.8);
+        pricePlane.setAttribute("position", { x: -1.3, y: 0, z: 0.3 });
+        pricePlane.setAttribute("rotation", { x: -90, y: 0, z: 0 });
+
+        const price = document.createElement("a-entity");
+        price.setAttribute("id", `price-${dish.id}`);
+        price.setAttribute("position", { x: 0.03, y: 0.05, z: 0.1 });
+        price.setAttribute("rotation", { x: 0, y: 0, z: 0 });
+        price.setAttribute("text", {
+          font: "mozillavr",
+          color: "white",
+          width: 3,
+          align: "center",
+          value: `Only \n $${dish.price}`,
+        });
+        pricePlane.appendChild(price);
+
+        marker.appendChild(pricePlane);
+      }
     });
   },
   async getDishes() {
--- js/markerHandler.js
+++ js/markerHandler.js
@@ -1,45 +1,108 @@
+let tableNo = null;
+
 AFRAME.registerComponent("markerhandler", {
   init: async function () {
+    if (tableNo === null) {
+      this.askTableNo();
+    }
 
     this.el.addEventListener("markerFound", () => {
-      console.log("marker is found")
-      this.handleMarkerFound();
+      console.log("marker is found");
+      if (tableNo !== null) {
+        const markerId = this.el.id;
+
+        this.handleMarkerFound(dishes, markerId);
+      }
     });
 
     this.el.addEventListener("markerLost", () => {
-      console.log("marker is lost")
+      console.log("marker is lost");
       this.handleMarkerLost();
     });
   },
-  handleMarkerFound: function () {
+  handleMarkerFound: function (dishes, markerId) {
     // Changing button div visibility
-    var buttonDiv = document.getElementById("button-div");
-    buttonDiv.style.display = "flex";
-
-    var ratingButton = document.getElementById("rating-button");
-    var orderButtton = document.getElementById("order-button");
-
-    // Handling Click Events
-    ratingButton.addEventListener("click", function () {
+    const todaysDate = new Date();
+    const todaysDay = todaysDate.getDay();
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const dish = dishes.filter((dish) => dish.id === markerId)[0];
+    if (dish.unavailable_days.includes(days[todaysDay])) {
       swal({
         icon: "warning",
-        title: "Rate Dish",
-        text: "Work In Progress"
+        title: dish.dish_name.toUpperCase(),
+        text: "This dish is not available today.",
+        timer: 2500,
+        buttons: false,
       });
-    });
+    } else {
+      const model = document.querySelector(`#model-${dish.id}`);
+      model.setAttribute("position", dish.model_geometry.position);
+      model.setAttribute("rotation", dish.model_geometry.rotation);
+      model.setAttribute("scale", dish.model_geometry.scale);
 
-    orderButtton.addEventListener("click", () => {
-      swal({
-        icon: "https://i.imgur.com/4NZ6uLY.jpg",
-        title: "Thanks For Order!",
-        text: "Your order will be served soon at your table!"
+      model.setAttribute("visible", true);
+      const ingrContainer = document.querySelector(`#mainplane-${dish.id}`);
+      ingrContainer.setAttribute("visible", true);
+
+      const buttonDiv = document.getElementById("button-div");
+      buttonDiv.style.display = "flex";
+
+      const ratingButton = document.getElementById("rating-button");
+      const orderButtton = document.getElementById("order-button");
+
+      // Handling Click Events
+      ratingButton.addEventListener("click", function () {
+        swal({
+          icon: "warning",
+          title: "Rate Dish",
+          text: "Work In Progress",
+        });
       });
-    });
+
+      orderButtton.addEventListener("click", () => {
+        swal({
+          icon: "https://i.imgur.com/4NZ6uLY.jpg",
+          title: "Thanks For Order!",
+          text: "Your order will be served soon at your table!",
+        });
+      });
+
+      const priceplane = document.querySelector(`#priceplane-${dish.id}`);
+      priceplane.setAttribute("visible", true);
+    }
   },
 
   handleMarkerLost: function () {
     // Changing button div visibility
-    var buttonDiv = document.getElementById("button-div");
+    const buttonDiv = document.getElementById("button-div");
     buttonDiv.style.display = "none";
-  }
+  },
+
+  askTableNo() {
+    const iconUrl =
+      "https://raw.githubusercontent.com/whitehatjr/menu-card-app/main/hunger.png";
+    swal({
+      title: "Welcome to Hunger!",
+      icon: iconUrl,
+      content: {
+        element: "input",
+        attributes: {
+          placeholder: "Type your Table No here",
+          type: "number",
+          min: 1,
+        },
+      },
+      closeOnClickOutside: false,
+    }).then((inputVal) => {
+      tableNo = inputVal;
+    });
+  },
 });
